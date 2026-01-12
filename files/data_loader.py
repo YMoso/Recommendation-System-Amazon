c5b712645bb27a0b13251220afa00337e80fa12e
najgorzejstudio <najgorzej111@gmail.com>
2026-01-12 14:49:40 +0100

    feat: incorporated embedding recommendation, added files to config, added indexes to data_loader

--- files/data_loader.py
+++ files/data_loader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import joblib
+import faiss
+import pickle
 from CONFIG.config import Config
 
 
@@ -12,6 +14,14 @@ class DataStore:
         self.svdpp = None
         self.knn = None
 
+        self.product_index = None
+        self.user_index = None
+        self.item_mapping_path = None
+        self.item_id_to_idx = None
+        self.idx_to_item_id = None
+        self.embedding_dim = None
+        self.user_ids = None
+
     def load_all(self):
         self.interactions_df = pd.read_parquet(Config.INTERACTIONS_PATH)
         self.user_stats_df = pd.read_parquet(Config.USER_STATS_PATH)
@@ -19,6 +29,21 @@ class DataStore:
         self.best_svd = joblib.load(Config.SVD_PATH)
         self.svdpp = joblib.load(Config.SVDPP_PATH)
         self.knn = joblib.load(Config.KNN_PATH)
+
+        self.product_index = faiss.read_index(Config.PRODUCT_INDEX)
+        self.user_index = faiss.read_index(Config.USER_INDEX)
+        self.item_mapping_path = Config.PRODUCT_MAPPING
+        self.item_id_to_idx = {}
+        self.idx_to_item_id = {}
+        self.embedding_dim = None
+        self.user_ids = [p.stem for p in Config.USER_EMBEDDINGS_PATH.glob("*.npy")]
+
+        with open(self.item_mapping_path, "rb") as f:
+            data = pickle.load(f)
+            self.item_id_to_idx = data["product_id_to_idx"]
+            self.idx_to_item_id = data["idx_to_product_id"]
+            self.embedding_dim = data["embedding_dim"]
+
 
     def get_user_stats(self, user_id):
         row = self.user_stats_df[self.user_stats_df["user_id"] == user_id]
