bc703198553770091146859dd5cedfce823aab54
Yuriy Mosorov <252514@edu.p.lodz.pl>
2026-01-14 01:26:53 +0100

    feat: Added the embedding for cold user recommendations

--- CONFIG/config.py
+++ CONFIG/config.py
@@ -7,19 +7,13 @@ class Config:
     INTERACTIONS_PATH = os.path.join(DATA_DIR, "user_interactions_all.parquet")
     USER_STATS_PATH = os.path.join(DATA_DIR, "user_stats.parquet")
     ITEMS_META_PATH = os.path.join(DATA_DIR, "items_meta.parquet")
-    CD_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "cd_vinyl/embeddings_by_product_id")
-    MOVIES_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "movies_tv/embeddings_by_product_id")
-    SOFTWARE_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "software/embeddings_by_product_id")
-    GAMES_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "video_games/embeddings_by_product_id")
-    USER_EMBEDDINGS_PATH = os.path.join(DATA_DIR, "user_embeddings_lt20")
 
     MODELS_DIR = os.path.join(BASE_DIR, "../models")
     SVD_PATH = os.path.join(MODELS_DIR, "svd.pkl")
     SVDPP_PATH = os.path.join(MODELS_DIR, "svdpp.pkl")
     KNN_PATH = os.path.join(MODELS_DIR, "knn.pkl")
-    PRODUCT_INDEX = os.path.join(MODELS_DIR, "product_index.faiss")
-    PRODUCT_MAPPING = os.path.join(MODELS_DIR, "product_mappings.pkl")
-    USER_INDEX = os.path.join(MODELS_DIR, "user_index.faiss")
+    ITEM_INDEX_PATH = os.path.join(MODELS_DIR, "item_index.pkl")
+    ITEM_MAPPING_PATH = os.path.join(MODELS_DIR, "product_mappings.pkl")
 
     HYBRID_WEIGHTS = {"svd": 0.3, "svdpp": 0.5, "knn": 0.2 }
 
@@ -27,3 +21,4 @@ class Config:
     POPULAR_MIN_COUNT = 10
     MAX_WORKERS = 8
     CACHE_SIZE = 10_000
+    COLD_START_SAMPLE = 200000
